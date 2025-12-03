732b22ca29e3ecdc9c77da8177a9adff95029dd6
Nabb <ca202329593@alm.buap.mx>
2025-12-02 22:36:55 -0600

    host

--- control_escolar_desit_api/settings.py
+++ control_escolar_desit_api/settings.py
@@ -7,7 +7,13 @@ SECRET_KEY = '-_&+lsebec(whhw!%n@ww&1j=4-^j_if9x8$q778+99oz&!ms2'
 
 DEBUG = True  # en desarrollo
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    'nabb.pythonanywhere.com',  
+    '127.0.0.1',              
+    'localhost',               
+    '.pythonanywhere.com',     
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
